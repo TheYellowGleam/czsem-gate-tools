dc020d93f20f7050c1d04257101e79d09cb16297
dedekj <dedekj@gmail.com>
2017-01-20 17:51:07 +0100

    minor cleaning

--- modules/netgraph-tree-viewer/src/main/java/czsem/netgraph/batik/BatikTreeBuilder.java
+++ modules/netgraph-tree-viewer/src/main/java/czsem/netgraph/batik/BatikTreeBuilder.java
@@ -1,6 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2017 Datlowe and/or its affiliates. All rights reserved.
- ******************************************************************************/
 package czsem.netgraph.batik;
 
 import java.awt.Dimension;
